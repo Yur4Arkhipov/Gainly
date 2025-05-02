8a4a7af8dedff8058107efc9b73e1a59d757789b
Yur4Arkhipov <yura4rkhipov@yandex.ru>
2025-05-02 11:37:32 +0300

    add simple navigation

--- app/src/main/java/com/example/gainly/App.kt
+++ app/src/main/java/com/example/gainly/App.kt
@@ -1,7 +1,6 @@
 package com.example.gainly.com.example.gainly
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.example.gainly.navigation.Navigation
 
 @Composable
