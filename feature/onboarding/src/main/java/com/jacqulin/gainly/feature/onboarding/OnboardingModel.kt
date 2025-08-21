6778249a3b88aaebf4b7c10188350267f3070ca3
Yura <Yura.arkhipov.2004@mail.ru>
2025-08-21 19:09:17 +0300

    [feature_onboarding] change back image

--- feature/onboarding/src/main/java/com/jacqulin/gainly/feature/onboarding/OnboardingModel.kt
+++ feature/onboarding/src/main/java/com/jacqulin/gainly/feature/onboarding/OnboardingModel.kt
@@ -1,6 +1,7 @@
 package com.jacqulin.gainly.feature.onboarding
 
 import androidx.annotation.DrawableRes
+import com.jacqulin.gainly.core.designsystem.R
 
 sealed class OnboardingModel(
     @DrawableRes val image: Int,
@@ -9,25 +10,25 @@ sealed class OnboardingModel(
 ) {
 
     data object FirstPage : OnboardingModel(
-        image = R.drawable.onboarding1,
+        image = R.drawable.onboarding_back,
         title = "Ставь цели",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet viverra duis bibendum nunc pellentesque."
     )
 
     data object SecondPage : OnboardingModel(
-        image = R.drawable.onboarding2,
+        image = R.drawable.onboarding_back,
         title = "Заряжайся силой",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet viverra duis bibendum nunc pellentesque."
     )
 
     data object ThirdPage : OnboardingModel(
-        image = R.drawable.onboarding1,
+        image = R.drawable.onboarding_back,
         title = "Правильно питайся",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet viverra duis bibendum nunc pellentesque."
     )
 
     data object FourthPage : OnboardingModel(
-        image = R.drawable.onboarding2,
+        image = R.drawable.onboarding_back,
         title = "Улучшай качество сна",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet viverra duis bibendum nunc pellentesque."
     )
